26f50e44e52ef9e065a6660914f07891980c0f16
indranilbarman85 <indranil.barman@gmail.com>
2026-08-16 15:50:56 -0400

    Add: majority-element-ii - submission-1

---
+++ Data Structures & Algorithms/majority-element-ii/submission-1.py
@@ -0,0 +1,5 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        from collections import Counter
+        threshold = len(nums) // 3
+        return [k for k, v in Counter(nums).items() if v > threshold]

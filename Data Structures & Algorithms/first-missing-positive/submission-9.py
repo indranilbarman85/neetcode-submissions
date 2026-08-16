25582ba10711d7b29d781058b1b1ab71063df644
indranilbarman85 <indranil.barman@gmail.com>
2026-08-16 17:38:37 -0400

    Add: first-missing-positive - submission-9

---
+++ Data Structures & Algorithms/first-missing-positive/submission-9.py
@@ -0,0 +1,11 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        
+        min_z=min([v for v in nums if v>=0], default=1)
+
+        if min_z>1 or  min_z is None:
+            return 1
+        while min_z in nums:
+            min_z+=1
+        
+        return min_z

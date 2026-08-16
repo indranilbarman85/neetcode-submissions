b31cb009619d630ee39cb9faf5a32ab14daab13d
indranilbarman85 <indranil.barman@gmail.com>
2026-08-15 23:20:03 -0400

    Add: longest-consecutive-sequence - submission-4

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-4.py
@@ -0,0 +1,43 @@
+class Solution:
+
+    def find(self,nums: List[int])->List[List[int]]:
+
+        group_set=[]
+        group_list=[]
+        nums_s=sorted(nums)
+        group_list.append(nums_s[0])
+        for i in range(0,len(nums_s)-1):
+            if nums_s[i+1]-nums_s[i]<=1:
+                group_list.append(nums_s[i+1])
+            else:
+                group_set.append(group_list.copy())
+                group_list.clear()
+                group_list.append(nums_s[i+1])
+        # group_list.append(nums_s[-1])
+        group_set.append(group_list.copy())
+            
+        return group_set
+
+            
+
+
+    def longestConsecutive(self, nums: List[int]) -> int:
+        
+        if len(nums)==0:
+            return 0
+        max_length=0
+        group_list=[]
+        nums_s=sorted(nums)
+        group_list.append(nums_s[0])
+        for i in range(0,len(nums_s)-1):
+            if nums_s[i+1]-nums_s[i]<=1:
+                group_list.append(nums_s[i+1])
+            else:
+                length=len(set(group_list))
+                max_length=length if max_length<length else max_length
+                group_list.clear()
+                group_list.append(nums_s[i+1])
+        length=len(set(group_list))
+        max_length=length if max_length<length else max_length    
+        return max_length
+        

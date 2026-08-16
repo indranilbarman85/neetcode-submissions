3e67387b90a3c16025d2c6607be54ca9a55f965d
indranilbarman85 <indranil.barman@gmail.com>
2026-08-16 19:11:24 -0400

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        i=0
+        j=len(s)-i-1
+        while i<len(s)//2:
+            
+            while not s[i].isalnum() and i<len(s)-1:
+                i+=1
+            while not s[j].isalnum() and j>=0:
+                j-=1
+            if s[i].lower()!=s[j].lower():
+                return False
+            i+=1
+            j-=1
+        return True
+            
+
+    
+        
